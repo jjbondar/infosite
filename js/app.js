89e199fb24e9cef37a83110c406ff04298e772ea
Andrey Kovalenko <gms.kov@gmail.com>
2019-07-27 19:47:29 +0300

    Add js file

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('body').scrollspy({target: ".navbar", offset: 50});
+
+
+    $("#navbarCollapse a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            let hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
